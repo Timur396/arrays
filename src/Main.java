dc3eb2e4401d1096edd783bb1c0a1989dbda7cb2
Аминов <aminov-timur92@mail.ru>
2022-10-13 15:35:25 +0400

    Второй коммит.

--- src/Main.java
+++ src/Main.java
@@ -89,6 +89,7 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0)
                 arr[i]+=1;
+
         }
         System.out.println(Arrays.toString(arr));
     }
