fcebb1db2ebd99e900fb0d8da2d61efde1da28ef
Аминов <aminov-timur92@mail.ru>
2022-10-13 15:34:16 +0400

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,98 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        //  task5();
+        // task6();
+        // task7();
 
-        System.out.println("Hello world!");
+
     }
 
+    public static void task1()
+    //Задача1.1
+    {
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 4;
+        weight[2] = 83;
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print(weight[i]);
+            if (i != weight.length - 1) {
+                System.out.print(", ");
+            }
+
+        }
+        System.out.println();
+
+        for (int i = weight.length - 1; i >= 0; i--) {
+            System.out.print(weight[i]);
+            if (i != 0) {
+                System.out.print(", ");
+
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task2()
+
+    //Задача1.2
+    {
+        double[] fraction = {1.57, 7.654, 9.986};
+        for (int i = 0; i < fraction.length; i++) {
+            System.out.print(fraction[i]);
+            if (i != fraction.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = fraction.length - 1; i >= 0; i--) {
+            System.out.print(fraction[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+
+        }
+        System.out.println();
+    }
+
+
+    public static void task3() {
+//Задача1.3
+        String[] fraction = {"one", "two"};
+        for (int i = 0; i < fraction.length; i++) {
+            System.out.print(fraction[i]);
+            if (i != fraction.length - 1) {
+                System.out.print(", ");
+            }
+
+        }
+        System.out.println();
+        for (int i = fraction.length - 1; i >= 0; i--) {
+            System.out.print(fraction[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+
+        }
+        System.out.println();
+    }
+
+    public static void task4() {
+        int[] arr = {1, 2, 3};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0)
+                arr[i]+=1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
+
+
+
